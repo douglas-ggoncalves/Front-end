8ae744280734eb89bfa25892c9c9bbae9c611d3d
Douglas Gonçalves <douglasdepaula10@hotmail.com>
2022-01-08 22:17:02 -0300

    .

---
+++ Vue.js/pokedex/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <input type="text" placeholder="Buscar Pokemon" v-model="inputSearch" />
+    <div v-for="pokemon in myFunction" :key="pokemon.url">
+      <Pokemon :pokemon="pokemon" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Pokemon from "./components/Pokemon.vue";
+//import _ from 'lodash'
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      teste: "",
+      inputSearch: "",
+      pokemons: [
+        {
+          name: "",
+          url: "",
+        },
+      ],
+    };
+  },
+  components: {
+    Pokemon,
+  },
+  created() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200").then((res) => {
+        this.pokemons = res.data.results;
+      });
+  },
+  computed: {
+    myFunction: function () {
+      if(this.inputSearch.trim() == ''){
+        return this.pokemons;
+      }
+      else{
+        return this.pokemons.filter(element =>
+          element.name.match(this.inputSearch)
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+input {
+  background: none;
+  border: 2px solid black;
+  outline: none;
+  color: gray;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 33%;
+  border-radius: 24px;
+  padding: 0.5rem 0.7rem;
+}
+</style>
---
+++ Vue.js/pokedex/src/components/Pokemon.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="myDiv">
+        <h3>{{ pokemon.name }}</h3>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/201.png" style="" alt="">
+        <h3>{{ pokemon.url }}</h3>
+        <button>Mudar Sprite</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props :{
+        pokemon: Object
+    }
+}
+</script>
+
+<style scoped>
+    div{
+        border: 1px solid black;
+        max-width: 95%;
+        margin: 1rem auto;
+        padding: 1rem;
+    }
+
+    button{
+        padding: .5rem .7rem;
+        cursor: pointer;
+        background: none;
+        outline: none;
+        border: 1px solid gray;
+    }
+
+    button:hover{
+        background: gray;
+        color: white;
+    }
+</style>
