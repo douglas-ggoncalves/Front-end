f3760caa3e59c9374422b18e12c1492fbf390fe3
Douglas Gonçalves <douglasdepaula10@hotmail.com>
2022-01-16 11:19:25 -0300

    .

---
+++ Vue.js/conversor/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <Coin :Coin="teste" />
+  </div>
+</template>
+
+<script>
+import Coin from "./components/Coin.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      teste: 'sasd',
+      array: [
+        {
+          nomeElementos: "<AFN>Afghani do Afeganistão</AFN>",
+        },
+        {
+          nomeElementos: "<MGA>Ariary Madagascarense</MGA>",
+        },
+        {
+          nomeElementos: "<THB>Baht Tailandês</THB>",
+        },
+      ],
+    }
+  },
+  components: {
+    Coin,
+  },
+  created() {
+    axios
+      .get("https://economia.awesomeapi.com.br/xml/available/uniq")
+      .then((res) => {
+        console.log(res.data)
+        console.log(this.array)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+
+<style>
+html {
+  height: 100%;
+  background-color: rgb(32, 33, 36);
+  display: flex;
+  justify-content: center;
+}
+
+div#app {
+  text-align: center;
+  margin-top: 5rem;
+}
+
+h1 {
+  color: gray;
+}
+</style>
---
+++ Vue.js/conversor/src/components/Coin.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="elements">
+     <h4>1 Real brasileiro equivale a </h4>
+     <h3>0,18 Dólar Americano</h3>
+
+    <div class="divConvert">
+        <input type="text" value="1">
+
+        <select name="select" >
+            <option value="valor1" selected>{{ teste }}</option>
+            <option value="valor1">Real Brasileiro</option>
+        </select>
+    </div>
+
+    <div class="divConvert">
+        <input type="text" value="5.28">
+
+        <select name="select">
+            <option value="valor1" selected>Dólar Americano</option>
+        </select>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            Coin: Object
+        }
+    }, props: {
+        teste: String
+    }
+};
+</script>
+
+<style scoped>
+    *{
+        outline: none;
+    }
+    div.elements{
+        display: inline-block;
+        margin-left: auto;
+        margin-right: auto;
+        -webkit-box-shadow: 5px 5px 12px -1px rgba(0,0,0,0.89); 
+        box-shadow: 5px 4px 12px -1px rgba(0,0,0,0.89);
+        padding: .5rem 1.5rem;
+    }
+
+    input{
+        background: none;
+        display: inline-block !important;
+        border: 1px solid rgb(133, 133, 133);
+        border-radius: 6px;
+        height: 100%;
+        padding-left: .7rem;
+        color: rgba(255, 255, 255, 0.627);
+        width: 120px;
+    }
+
+    h3{
+        color: rgba(255, 255, 255, 0.815);
+    }
+
+    h4{
+        color: rgba(255, 255, 255, 0.527);
+    }
+
+    select{
+        cursor: pointer;
+        background: none !important;
+        color: rgba(255, 255, 255, 0.527);
+        height: 100% !important;
+        margin-left: .5rem;
+        border: 1px solid rgb(133, 133, 133) !important;
+        padding-top: .1rem;
+        padding-bottom: .1rem;
+        width: 140px;
+        border-radius: 6px;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        background: rgb(32, 33, 36) !important ;
+    }
+
+    div.divConvert{
+        height: 26px;
+        margin-bottom: .8rem;
+    }
+</style>
