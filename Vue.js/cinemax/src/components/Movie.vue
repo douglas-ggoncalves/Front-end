77d5683d6a416f4eda085bed7914fe7aadc8a20c
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-02-02 15:37:33 -0300

    .

---
+++ Vue.js/cinemax/src/components/Movie.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>Filmes</nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  margin: 0;
+  width: 100%;
+  display: flex;
+}
+nav {
+  background-color: black;
+  width: 100%;
+}
+</style>
