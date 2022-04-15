e3645d67ebcb4fb5601267dc4d04ccaed17a961e
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-04-15 20:54:34 -0300

    .

---
+++ Construction/js/script.js
@@ -0,0 +1,122 @@
+
+var largura;
+
+if(largura == undefined){
+    largura = window.innerWidth;
+    console.log(largura)
+    if(largura < 768) {
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 30,
+            //freeMode: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            }
+        });
+
+        var mySwiperSponsors = new Swiper(".mySwiperSponsors", {
+            loop: true,
+            slidesPerView: 2,
+            spaceBetween: 30
+        });
+    }
+    if(largura > 768 && largura < 992){
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            slidesPerView: 2,
+            spaceBetween: 30,
+            //freeMode: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            }
+        });
+
+        var mySwiperSponsors = new Swiper(".mySwiperSponsors", {
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 30
+        });
+    }
+    if(largura > 992){
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 30,
+            //freeMode: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            }
+        });
+
+        var mySwiperSponsors = new Swiper(".mySwiperSponsors", {
+            loop: true,
+            slidesPerView: 5,
+            spaceBetween: 30
+        });
+    }   
+}
+
+
+window.addEventListener("resize", () => {
+    largura = window.innerWidth;
+    
+    if(largura < 768) {
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 30,
+            //freeMode: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            }
+        });
+
+        var mySwiperSponsors = new Swiper(".mySwiperSponsors", {
+            loop: true,
+            slidesPerView: 2,
+            spaceBetween: 30
+        });
+    }
+    if(largura > 768 && largura < 992){
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            slidesPerView: 2,
+            spaceBetween: 30,
+            //freeMode: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            }
+        });
+
+        var mySwiperSponsors = new Swiper(".mySwiperSponsors", {
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 30
+        });
+    }
+    if(largura > 992){
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 30,
+            //freeMode: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            }
+        });
+
+        var mySwiperSponsors = new Swiper(".mySwiperSponsors", {
+            loop: true,
+            slidesPerView: 5,
+            spaceBetween: 30
+        });
+    }
+  });
+
