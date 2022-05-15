611e2111dcc92a079a1a37cb8f0045024eb7e6a7
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-05-15 00:40:02 -0300

    .

---
+++ Vue.js/curriculo/src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+      <v-main>
+        <v-container>Hello world</v-container>
+      </v-main>
+    </v-app>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+export default new Vuetify({
+  theme: { dark: true },
+})
+
+
+</script>
