19fc5e31a9bd96cee7b07d333d71edd85805e9e5
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-06-12 20:43:51 -0300

    .

--- Vue.js/calculadora/src/components/Calculator.vue
+++ Vue.js/calculadora/src/components/Calculator.vue
@@ -1,52 +1,58 @@
 <template>
-  <div id="main" @keypress="teste($event)">
-    <div >
+  <div id="main">
+    <div>
       <input type="text" :value="input" disabled />
 
       <div id="operators"></div>
 
       <div class="elements">
-        <button @click="number(7)">7</button>
-        <button @click="number(8)">8</button>
-        <button @click="number(9)">9</button>
-        <button @click="operator('+')">+</button>
+        <button id="7" @click="number(7)">7</button>
+        <button id="8" @click="number(8)">8</button>
+        <button id="9" @click="number(9)">9</button>
+        <button id="+" @click="operator('+')">+</button>
       </div>
 
       <div class="elements">
-        <button @click="number(4)">4</button>
-        <button @click="number(5)">5</button>
-        <button @click="number(6)">6</button>
-        <button @click="operator('-')">-</button>
+        <button id="4" @click="number(4)">4</button>
+        <button id="5" @click="number(5)">5</button>
+        <button id="6" @click="number(6)">6</button>
+        <button id="-" @click="operator('-')">-</button>
       </div>
 
       <div class="elements">
-        <button @click="number(1)">1</button>
-        <button @click="number(2)">2</button>
-        <button @click="number(3)">3</button>
-        <button @click="operator('x')">&times;</button>
+        <button id="1" @click="number(1)">1</button>
+        <button id="2" @click="number(2)">2</button>
+        <button id="3" @click="number(3)">3</button>
+        <button id="*" @click="operator('x')">&times;</button>
       </div>
 
       <div class="elements">
-        <button @click="operator('.')">.</button>
-        <button @click="number(0)">0</button>
+        <button id="." @click="operator('.')">.</button>
+        <button id="0" @click="number(0)">0</button>
         <button @click="operator('C')">C</button>
-        <button @click="operator('/')">&divide;</button>
+        <button id="/" @click="operator('/')">&divide;</button>
       </div>
 
       <div class="elements" id="buttonEqual">
-        <button @click="operator('=')">=</button>
+        <button id="Enter" @click="operator('=')">=</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+$(document.body).keyup(function(event) {
+  var element = document.getElementById(event.key);
+  element == undefined ? alert("O caractere informado não é aceito pela calculadora") : element.click();
+});
+
 import '../assets/css/style.css'
+import $ from 'jquery'
 export default {
   data() {
     return {
       input: '0'
-    };
+    }
   },
   methods: {
     number (value) {
@@ -59,17 +65,23 @@ export default {
     operator (value) {
       if (value == "C") {
         this.input = 0;
-      } else if(value == "="){
+      } else if(value == "=") {
         this.input = this.calc()
-      } else{
+      } else {
         if (value != ".") {
-          if(this.hasOperatorMath("+") || this.hasOperatorMath("-") || this.hasOperatorMath("/") || this.hasOperatorMath("x")){
-            this.input = this.calc()
+          if(this.hasOperatorMath("+") || this.hasOperatorMath("-") || this.hasOperatorMath("/") || this.hasOperatorMath("x") || this.hasOperatorMath("*")){
+            this.input = this.calc() + value
           } else{
             this.input += value;
           }
-        }
-        else if (value == ".") if(!this.verifyPoint()) this.input += value;
+        } else if (value == ".") 
+          if(this.hasOperatorMath("+") || this.hasOperatorMath("-") || this.hasOperatorMath("/") || this.hasOperatorMath("x") || this.hasOperatorMath("*")){
+            if(!this.verifyPoint(1)) this.input += value;
+          } 
+
+          else{ 
+            if(!this.verifyPoint()) this.input += value 
+          }
       }
     },
     calc(){
@@ -80,16 +92,25 @@ export default {
       if(this.input.toString().includes(operadorMath)) return true; // já tem a operação matemática na tela
       if(!this.input.toString().includes(operadorMath)) return false;
     },
-    verifyPoint(){
+    verifyPoint(teste){
+      var more, sub, mult, div;
+      if(this.input.toString().includes("+")) more = true;  
+      if(this.input.toString().includes("-")) sub = true;  
+      if(this.input.toString().includes("x")) mult = true;  
+      if(this.input.toString().includes("*")) mult = true;  
+      if(this.input.toString().includes("+")) div = true;  
+      
+      
+      //var stringSplit;
+      if(teste == undefined) {
+
+      }
+      console.log(this.input.toString().split('+'))
+      console.log(this.input.toString().split('+')[0])
+      console.log(this.input.toString().split('+')[1])
       if(this.input[this.input.length -1] == ".") return true;
-    },
-    teste(event){
-      if(event.key == 0 || event.key == 1 || event.key == 2 || event.key == 3 || event.key == 4 
-      || event.key == 5 || event.key == 6 || event.key == 7 || event.key == 8 || event.key == 9) 
-      this.number(event.key);
-      console.log("testado " + event.key)
     }
   },
- 
 };
+
 </script>
