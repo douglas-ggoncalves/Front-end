c6853c3f4e40283766b8ad9a3e79e89b99854383
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2022-06-11 23:47:10 -0300

    Update Calculator.vue

--- Vue.js/calculadora/src/components/Calculator.vue
+++ Vue.js/calculadora/src/components/Calculator.vue
@@ -1,34 +1,34 @@
 <template>
-  <div id="main">
-    <div>
+  <div id="main" @keypress="teste($event)">
+    <div >
       <input type="text" :value="input" disabled />
 
       <div id="operators"></div>
 
       <div class="elements">
-        <button @click="number(number7)">{{ number7 }}</button>
-        <button @click="number(number8)">{{ number8 }}</button>
-        <button @click="number(number9)">{{ number9 }}</button>
+        <button @click="number(7)">7</button>
+        <button @click="number(8)">8</button>
+        <button @click="number(9)">9</button>
         <button @click="operator('+')">+</button>
       </div>
 
       <div class="elements">
-        <button @click="number(number4)">{{ number4 }}</button>
-        <button @click="number(number5)">{{ number5 }}</button>
-        <button @click="number(number6)">{{ number6 }}</button>
+        <button @click="number(4)">4</button>
+        <button @click="number(5)">5</button>
+        <button @click="number(6)">6</button>
         <button @click="operator('-')">-</button>
       </div>
 
       <div class="elements">
-        <button @click="number(number1)">{{ number1 }}</button>
-        <button @click="number(number2)">{{ number2 }}</button>
-        <button @click="number(number3)">{{ number3 }}</button>
-        <button @click="operator('*')">&times;</button>
+        <button @click="number(1)">1</button>
+        <button @click="number(2)">2</button>
+        <button @click="number(3)">3</button>
+        <button @click="operator('x')">&times;</button>
       </div>
 
       <div class="elements">
         <button @click="operator('.')">.</button>
-        <button @click="number('0')">0</button>
+        <button @click="number(0)">0</button>
         <button @click="operator('C')">C</button>
         <button @click="operator('/')">&divide;</button>
       </div>
@@ -45,39 +45,51 @@ import '../assets/css/style.css'
 export default {
   data() {
     return {
-      input: 0,
-      number0: "0",
-      number1: "1",
-      number2: "2",
-      number3: "3",
-      number4: "4",
-      number5: "5",
-      number6: "6",
-      number7: "7",
-      number8: "8",
-      number9: "9",
+      input: '0'
     };
   },
   methods: {
-    operator: function (value) {
+    number (value) {
+      if (this.input == 0 && this.input.toString() != '0.') {
+        this.input = value;
+      } else {
+        this.input += value.toString();
+      }
+    },
+    operator (value) {
       if (value == "C") {
         this.input = 0;
-      } else if (value == "+") {
-        if(this.input.toString().includes('+')){
-          console.log('já tem mais')
-          this.input = eval(this.input)
-        } else{
-          this.input += value;
+      } else if(value == "="){
+        this.input = this.calc()
+      } else{
+        if (value != ".") {
+          if(this.hasOperatorMath("+") || this.hasOperatorMath("-") || this.hasOperatorMath("/") || this.hasOperatorMath("x")){
+            this.input = this.calc()
+          } else{
+            this.input += value;
+          }
         }
+        else if (value == ".") if(!this.verifyPoint()) this.input += value;
       }
     },
-    number: function (value) {
-      if (this.input == 0) {
-        this.input = value;
-      } else if (this.input != 0) {
-        this.input += value;
-      }
+    calc(){
+      if(this.input.toString().includes('x')) return eval(this.input.toString().replace("x","*"));
+      return eval(this.input);
     },
+    hasOperatorMath(operadorMath){
+      if(this.input.toString().includes(operadorMath)) return true; // já tem a operação matemática na tela
+      if(!this.input.toString().includes(operadorMath)) return false;
+    },
+    verifyPoint(){
+      if(this.input[this.input.length -1] == ".") return true;
+    },
+    teste(event){
+      if(event.key == 0 || event.key == 1 || event.key == 2 || event.key == 3 || event.key == 4 
+      || event.key == 5 || event.key == 6 || event.key == 7 || event.key == 8 || event.key == 9) 
+      this.number(event.key);
+      console.log("testado " + event.key)
+    }
   },
+ 
 };
 </script>
